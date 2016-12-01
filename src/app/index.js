3caae78ba850a11db8731d5e49b9b7a736715e1b
Chua Wei Kuan <khankuan@gmail.com>
2016-11-30 20:35:03 -0800

    Use onLoad for window

--- src/app/index.js
+++ src/app/index.js
@@ -8,15 +8,17 @@ import PageNotFound from './PageNotFound'
 import history from './history'
 import { pages } from 'build/config'
 
-render((
-  <Router history={history}>
-    <Route path=''>
-      {pages ? pages.map((page, i) => React.cloneElement(page, { key: i, component: App, page: page.props.component })) : null}
-      <Route path=':tag/browse' component={App} />
-      <Route path=':tag/browse/:mode' component={App} />
-      <Route path=':tag/:component' component={App} />
-      <Route path=':tag/:component/:story' component={App} />
-      <Route path='*' component={PageNotFound} />
-    </Route>
-  </Router>
-), document.getElementById('app'))
+window.onload = () => {
+  render((
+    <Router history={history}>
+      <Route path=''>
+        {pages ? pages.map((page, i) => React.cloneElement(page, { key: i, component: App, page: page.props.component })) : null}
+        <Route path=':tag/browse' component={App} />
+        <Route path=':tag/browse/:mode' component={App} />
+        <Route path=':tag/:component' component={App} />
+        <Route path=':tag/:component/:story' component={App} />
+        <Route path='*' component={PageNotFound} />
+      </Route>
+    </Router>
+  ), document.getElementById('app'))
+}
