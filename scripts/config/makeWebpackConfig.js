236952755928fad868d7e8f034bfb227698b93a0
Chua Wei Kuan <khankuan@gmail.com>
2017-02-01 16:01:00 +0800

    do not mangle variables name

--- scripts/config/makeWebpackConfig.js
+++ scripts/config/makeWebpackConfig.js
@@ -44,9 +44,7 @@ const prodPlugins = [
       screw_ie8: true, // React doesn't support IE8
       warnings: false
     },
-    mangle: {
-      screw_ie8: true
-    },
+    mangle: false,
     output: {
       comments: false,
       screw_ie8: true
