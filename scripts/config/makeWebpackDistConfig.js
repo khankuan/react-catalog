9595b4fdd8bd1a47977cf184f4fd4e96fa692df7
Chua Wei Kuan <khankuan@gmail.com>
2017-04-17 18:36:24 -0700

    fix(Dist): Fixed dist path

--- scripts/config/makeWebpackDistConfig.js
+++ scripts/config/makeWebpackDistConfig.js
@@ -1,12 +1,14 @@
 import webpack from 'webpack'
+import path from 'path'
 import makeWebpackConfig from './makeWebpackConfig'
 
 export default function makeWebpackDistConfig(opts) {
   const config = makeWebpackConfig(opts)
+  const outputDistDir = path.resolve(process.cwd(), `${outputPublicDir}/dist`)
 
   const { outputPublicDir } = opts
   config.output = {
-    path: `${outputPublicDir}/dist`,
+    path: outputDistDir,
     filename: '[name].js',
     libraryTarget: 'umd',
   }
